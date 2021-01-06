3db638d6d3895dd0a660b2b0c726cac712f1ed82
Victor <rusuvictor30@gmail.com>
2021-01-06 00:10:40 -0500

    SDK added

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Login With Facebook
+ * Description: This plugin will add a Login with Facebook Button
+ * Plugin URI: https://vicodemedia.com
+ * Author: Victor Rusu
+ * Version: 0.0.1
+**/
+
+//* Don't access this file directly
+defined( 'ABSPATH' ) or die();
+
+require_once 'Facebook/autoload.php';
